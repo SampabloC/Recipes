a82239dc8b7475561fbd188375c83720fffb3991
SampabloC <sampablo-jisc@outlook.com>
2022-01-30 22:42:57 -0600

    Add model and controller for Recipes Create backend methods, migration, add validations for recipes Methods to create, destroy and show Validations for name, instructions and ingredients

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::RecipesController < ApplicationController
+  def index
+    recipe = Recipe.all.order(created_at :desc)
+    render json: recipe
+  end
+
+  def create
+    recipe = Recipe.create!(recipe_params) # ! is for strong params, consult https://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+    if recipe
+      render json: recipe
+    else
+      render json: recipe_erros
+    end
+  end
+
+  def show
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
+  end
+
+  def destroy
+    recipe&.destroy # & is for ruby safe navigation operator wich avoid nill errors
+    render json: { message: 'Recipe deleted' }
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name, :image, :ingredients, :instructions)
+  end
+
+  def recipe
+    @recipe ||= Recipe.find(params[:id])
+  end
+end
